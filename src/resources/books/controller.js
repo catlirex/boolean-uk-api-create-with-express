1f473d163e5b403747dc14cf03963b56334d5133
Catherine Li <catherine.uk.20@gmail.com>
2021-08-03 11:53:40 +0100

    add controller error msg

--- src/resources/books/controller.js
+++ src/resources/books/controller.js
@@ -3,24 +3,50 @@ const Book = require("./model");
 const { createOneBook, getAllBooks, getBookById } = Book();
 
 const reqAllBooks = (req, res) => {
+  const queryContent = req.query;
+  if (Object.keys(queryContent).length !== 0)
+    return res.json({ Error: "DB not support Query, please retry" });
+
   getAllBooks((result) => {
     res.json({ AllBooks: result.rows });
   });
 };
 
 const reqBookById = (req, res) => {
+  const queryContent = req.query;
+  if (Object.keys(queryContent).length !== 0)
+    return res.json({ Error: "DB not support Query, please retry" });
+
   const { id } = req.params;
   getBookById(id, (result) => {
-    res.json({ result: result.rows });
+    if (result.rows.length !== 0) res.json({ result: result.rows });
+    else res.json({ ERROR: `Book id:${id} not found` });
   });
 };
 
 const addNewBook = (req, res) => {
   const newBook = req.body;
-  console.log(newBook);
-  createOneBook(newBook, (result) => {
-    res.json({ newBook: result.rows });
-  });
+  const newBookRequirement = [
+    "title",
+    "type",
+    "author",
+    "topic",
+    "publicationDate",
+  ];
+
+  const hasAllKeys = newBookRequirement.every((item) =>
+    newBook.hasOwnProperty(item)
+  );
+
+  if (hasAllKeys && Object.keys(newBook).length === newBookRequirement.length)
+    createOneBook(newBook, (result) => {
+      res.json({ newBook: result.rows });
+    });
+  else
+    res.json({
+      ERROR: "Info incorrect, please provide exact object(see requirement)",
+      newBookRequirement,
+    });
 };
 
 module.exports = { reqAllBooks, reqBookById, addNewBook };
--- src/resources/pets/controller.js
+++ src/resources/pets/controller.js
@@ -4,24 +4,43 @@ const Pet = require("./model");
 const { getAllPets, createOnePet, getPetById } = Pet();
 
 const reqAllPets = (req, res) => {
+  const queryContent = req.query;
+  if (Object.keys(queryContent).length !== 0)
+    return res.json({ Error: "DB not support Query, please retry" });
+
   getAllPets((result) => {
     res.json({ AllPets: result.rows });
   });
 };
 
 const reqPetById = (req, res) => {
+  const queryContent = req.query;
+  if (Object.keys(queryContent).length !== 0)
+    return res.json({ Error: "DB not support Query, please retry" });
+
   const { id } = req.params;
   getPetById(id, (result) => {
-    res.json({ result: result.rows });
+    if (result.rows.length !== 0) res.json({ result: result.rows });
+    else res.json({ ERROR: `Pets id:${id} not found` });
   });
 };
 
 const addNewPet = (req, res) => {
   const newPet = req.body;
-  console.log(newPet);
-  createOnePet(newPet, (result) => {
-    res.json({ newPet: result.rows });
-  });
+  const newPetRequirement = ["name", "age", "type", "breed", "microchip"];
+
+  const hasAllKeys = newPetRequirement.every((item) =>
+    newPet.hasOwnProperty(item)
+  );
+  if (hasAllKeys && Object.keys(newPet).length === newPetRequirement.length)
+    createOnePet(newPet, (result) => {
+      res.json({ newPet: result.rows });
+    });
+  else
+    res.json({
+      ERROR: "Info incorrect, please provide exact object(see requirement)",
+      newPetRequirement,
+    });
 };
 
 module.exports = { reqAllPets, reqPetById, addNewPet };
